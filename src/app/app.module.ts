23146bb3ffb1d0e2a25e5782dad96b409fee6c33
Kumar <kesinenivenkatasai.kumar@kochgs.com>
2023-05-30 19:58:12 +0530

    modified module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SecondComponent } from './second/second.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SecondComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
